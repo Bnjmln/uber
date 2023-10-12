fd76c1b05c4af27b1fc8881f4ed8669884448d24
Bnjmln <107646409+Bnjmln@users.noreply.github.com>
2023-10-12 20:10:37 -0300

    Update register.module.ts

--- src/app/paginas/register/register.module.ts
+++ src/app/paginas/register/register.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { FormsModule } from '@angular/forms';     
 import { RegisterPageRoutingModule } from './register-routing.module';
-
 import { RegisterPage } from './register.page';
+//app.module.ts
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegisterPageRoutingModule
+    RegisterPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [RegisterPage]
 })
